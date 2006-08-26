fa578b744ff50f7c51b2c0606a803abff08358b8
Vidar Wahlberg <canidae@exent.net>
2006-08-26 20:33:06 +0200

    Fixed a bit more on the "fft" demo. It's actually almost working.

--- libari/demos/fft.py
+++ libari/demos/fft.py
@@ -20,29 +20,42 @@ class FFT(libari.demos.base.Base):
 		ar = AudioReader()
 		while True:
 			if self.drawable:
-				(left, right) = ar.read()
-				left = left * 1575 / 32768 + 1575
-				x = left % 105
-				y = left / 105
-				self.canvas.setpixel(x, y, 99)
-				#self.canvas.setpixel(16, 15 - right, 99)
+				ar.read()
+				minv = 0
+				maxv = 4194304
+				#minv = min(ar.data[0:105])
+				#maxv = max(ar.data[0:105])
+				step = 30.0 / (maxv - minv)
+				#print minv
+				#print maxv
+				#print step
+				#print "--"
+				for x in range(0, 105, 1):
+					self.canvas.setpixel(x, 30 - (ar.data[x] - minv) * step, 99)
 				self.canvas.update()
-				self.canvas.setpixel(x, y, 0)
-				#self.canvas.setpixel(16, 15 - right, 0)
+				for x in range(0, 105, 1):
+					self.canvas.setpixel(x, 30 - (ar.data[x] - minv) * step, 0)
+				#self.canvas.setpixel(x, y, 99)
+				#self.canvas.update()
+				#self.canvas.setpixel(x, y, 0)
 				#ar.quit()
 				#self.stop()
-			self.sleep()
+			#self.sleep()
 
 class AudioReader:
 	def __init__(self):
 		# python loves self!
 		self.dsp = ossaudiodev.open('/dev/dsp', 'r')
-		self.dsp.setparameters(ossaudiodev.AFMT_S16_LE, 1, 44100)
+		self.dsp.setparameters(ossaudiodev.AFMT_S16_LE, 2, 44100)
+		self.lol = 5000.0
 
 	def read(self):
-		self.data = array.array('h', self.dsp.read(256))
-		self.data = 10 * numarray.log10(abs(numarray.fft.fft(self.data)))
-		print self.data
+		self.data = array.array('h', self.dsp.read(1024))
+		#x = numarray.arange(256.0)
+		#self.data = numarray.sin(2 * math.pi * (1250.0 / self.lol) * x) + numarray.sin(2 * math.pi * (625.0 / self.lol) * x)
+		#self.lol += 1
+		#self.data = 10 * numarray.log10(1e-20 + abs(numarray.fft.fft(self.data)))
+		self.data = 1e-20 + abs(numarray.fft.fft(self.data))
 		#return (self.data[0], self.data[1])
 		#print self.data
 		#print self.data[0]
